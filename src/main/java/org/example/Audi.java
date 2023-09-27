14890baac3e9a56a537d3432db7bede9c752679d
saivelamuri <saivelamuri26@gmail.com>
2023-09-27 15:26:00 +0200

    BMW updated

--- src/main/java/org/example/Audi.java
+++ src/main/java/org/example/Audi.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Audi {
-  public static void main(String[] args}
-  System.out.println("Audi!");
+  public static void main(String[] args)
+    {
+        System.out.println("Audi!");
+    }
+
 }
--- src/main/java/org/example/BMW.java
+++ src/main/java/org/example/BMW.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class BMW {
+public static void main(String[] args){
+
+    System.out.println("BMW!");
+}
 }
